9ee7c329ea5da2463a7fff9f38d26afe151a875f
Nailul Firdaus <nail.nafir@gmail.com>
2020-12-28 15:07:26 +0700

    menambahkan code model food

--- app/Models/Food.php
+++ app/Models/Food.php
@@ -2,10 +2,38 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Food extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'picturePath', 'name', 'description', 'ingredients', 'location', 'price', 'discount', 'rate', 'types'
+    ];
+
+    public function toArray() {
+        $toArray = parent::toArray();
+        $toArray['picturePath'] = $this->picturePath;
+        return $toArray;
+    }
+
+    public function getCreatedAtAttribute($created_at)
+    {
+        return Carbon::parse($created_at)
+            ->getPreciseTimestamp(3);
+    }
+    
+    public function getUpdatedAtAttribute($updated_at)
+    {
+        return Carbon::parse($updated_at)
+            ->getPreciseTimestamp(3);
+    }
+
+    public function getPicturePathAttribute() {
+        return url('') . Storage::url($this->attributes['picturePath']);
+    }
 }
