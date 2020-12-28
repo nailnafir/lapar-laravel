4b5aec91c184deddc3809012cd9d6e0293964388
Nailul Firdaus <nail.nafir@gmail.com>
2020-12-28 18:31:28 +0700

    membuat API register

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -2,15 +2,19 @@
 
 namespace App\Http\Controllers\API;
 
+use Exception;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use App\Actions\Fortify\PasswordValidationRules;
 
 class UserController extends Controller
 {
+    use PasswordValidationRules;
+
     public function login(Request $request) {
         try {
             // validasi input
@@ -33,7 +37,7 @@ class UserController extends Controller
                 throw new \Exception('Kesalahan data');
             }
 
-            // jika berhasil, maka sekalian login
+            // jika berhasil, masuk login
             $tokenResult = $user->createToken('authToken')->plainTextToken;
             return ResponseFormatter::success([
                 'access_token' => $tokenResult,
@@ -42,9 +46,57 @@ class UserController extends Controller
             ], 'Berhasil');
         } catch (Exception $errors) {
             return Response()->json([
-                'message' => 'Ada kesalahan',
+                'message' => 'Data yang diberikan tidak sesuai',
                 'errors' => $errors->validator->errors(),
             ], 500);
+        }
+    }
+
+    public function register(Request $request) {
+        try {
+            // validasi input
+            $request->validate([
+                'name' => 'required|string|max:255',
+                'email' => 'required|string|email|max:255|unique:users',
+                'password' => $this->passwordRules(),
+                'address' => 'required|string',
+                'houseNumber' => 'required|string|max:255',
+                'rtrw' => 'required|string|max:255',
+                'subDistrict' => 'required|string|max:255',
+                'district' => 'required|string|max:255',
+                'city' => 'required|string|max:255',
+                'phoneNumber' => 'required|string|max:255',
+            ]);
+
+            // buat user baru
+            User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'gender' => $request->gender,
+                'address' => $request->address,
+                'houseNumber' => $request->houseNumber,
+                'rtrw' => $request->rtrw,
+                'subDistrict' => $request->subDistrict,
+                'district' => $request->district,
+                'city' => $request->city,
+                'phoneNumber' => $request->phoneNumber,
+                'password' => Hash::make($request->password),
+            ]);
+
+            // buatkan user baru token untuk masuk login
+            $user = User::where('email', $request->email)->first();
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
+
+            return ResponseFormatter::success([
+                'access_token' => $tokenResult,
+                'token_type' => 'Bearer',
+                'user' => $user,
+            ], 'Pengguna Berhasil Terdaftar');
+        } catch (Exception $errors) {
+            return Response()->json([
+                'message' => 'Data yang diberikan tidak sesuai',
+                'errors' => $errors->validator->errors(),
+            ], 422);
         }
     }
 }
