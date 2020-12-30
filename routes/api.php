9149bffa023f7407e1e21a60ccd8cd01faecbbd9
Nailul Firdaus <nail.nafir@gmail.com>
2020-12-30 15:01:01 +0700

    membuat route API transaction update

--- routes/api.php
+++ routes/api.php
@@ -25,6 +25,7 @@ Route::middleware('auth:sanctum')->group(function() {
     Route::post('logout', [UserController::class, 'logout']);
 
     Route::get('transaction', [TransactionController::class, 'all']);
+    Route::post('transaction/{id}', [TransactionController::class, 'update']);
 });
 
 // tidak harus login
