2d7bffa3ff3dee303ec81340d075c6f0cc680215
Nailul Firdaus <nail.nafir@gmail.com>
2020-12-30 14:48:56 +0700

    membuat API transaction

---
+++ app/Http/Controllers/API/TransactionController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\Transaction;
+use Illuminate\Http\Request;
+use App\Helpers\ResponseFormatter;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class TransactionController extends Controller
+{
+    public function all(Request $request) {
+
+        // membuat filtering
+        $id = $request->input('id');
+        $limit = $request->input('limit', 6);
+        $food_id = $request->input('food_id');
+        $status = $request->input('status');
+
+        if ($id) {
+            $transaction = Transaction::with(['food', 'user'])->find($id);
+
+            if ($transaction) {
+                return ResponseFormatter::success(
+                    $transaction, 
+                    'Berhasil mengambil data transaksi'
+                );
+            } else {
+                return ResponseFormatter::error(
+                    null, 
+                    'Data transaksi tidak ada', 
+                    404
+                );
+            }
+        }
+
+        // ambil transaksi milik dia saja (yang sedang login)
+        $transaction = Transaction::with(['food', 'user'])->where('user_id', Auth::user()->id);
+
+        if($food_id) {
+            $transaction->where('food_id', $food_id);
+        }
+
+        if($status) {
+            $transaction->where('status', $status);
+        }
+
+        return ResponseFormatter::success(
+            $transaction->paginate($limit),
+            'Berhasil ambil data list transaksi'
+        );
+    }
+}
