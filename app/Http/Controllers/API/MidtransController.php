ee3881ebcc0290fabb872af3fbf15a7a5aa502f2
Nailul Firdaus <nail.nafir@gmail.com>
2021-01-01 20:48:58 +0700

    membuat API midtrans callback

---
+++ app/Http/Controllers/API/MidtransController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Midtrans\Config;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Transaction;
+use Midtrans\Notification;
+
+class MidtransController extends Controller {
+    public function callback(Request $request) {
+
+        //set konfigurasi midtrans
+        Config::$serverKey = config('services.midtrans.serverKey');
+        Config::$isProduction = config('services.midtrans.isProduction');
+        Config::$isSanitized = config('services.midtrans.isSanitized');
+        Config::$is3ds = config('services.midtrans.is3ds');
+
+        // buat instance midtrans notification
+        $notification = new Notification();
+
+        // assign ke variable untuk memudahkan coding
+        $status = $notification->transaction_status;
+        $type = $notification->payment_type;
+        $fraud = $notification->fraud_status;
+        $order_id = $notification->order_id;
+
+        // cari transaksi berdasarkan id
+        $transaction = Transaction::findOrFail($order_id);
+
+        // handle notifikasi status midtrans
+        if ($status == 'capture') {
+            if ($type == 'credit_card') {
+                if ($fraud == 'challenge') {
+                    $transaction->status = 'Tertunda';
+                } else {
+                    $transaction->status = 'Sukses';
+                }
+            }
+        } else if ($status == 'settlement') {
+            $transaction->status = 'Sukses';
+        } else if ($status == 'pending') {
+            $transaction->status = 'Tertunda';
+        } else if ($status == 'deny') {
+            $transaction->status = 'Dibatalkan';
+        } else if ($status == 'expire') {
+            $transaction->status = 'Dibatalkan';
+        } else if ($status == 'cancel') {
+            $transaction->status = 'Dibatalkan';
+        }
+
+        // simpan transaksi
+        $transaction->save();
+
+        // Kirimkan email
+        if ($transaction) {
+            if ($status == 'capture' && $fraud == 'accept') {
+                //
+            } else if ($status == 'settlement') {
+                //
+            } else if ($status == 'success') {
+                //
+            } else if ($status == 'capture' && $fraud == 'challenge') {
+                return response()->json([
+                    'meta' => [
+                        'code' => 200,
+                        'message' => 'Midtrans Payment Challenge'
+                    ]
+                ]);
+            } else {
+                return response()->json([
+                    'meta' => [
+                        'code' => 200,
+                        'message' => 'Midtrans Payment not Settlement'
+                    ]
+                ]);
+            }
+
+            return response()->json([
+                'meta' => [
+                    'code' => 200,
+                    'message' => 'Midtrans Notification Success'
+                ]
+            ]);
+        }
+    }
+}
