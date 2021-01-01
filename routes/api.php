b84e29381e893e83da777f5a26c761ef4e4c715a
Nailul Firdaus <nail.nafir@gmail.com>
2021-01-01 20:51:51 +0700

    membuat route API midtrans callback

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\FoodController;
+use App\Http\Controllers\API\MidtransController;
 use App\Http\Controllers\API\UserController;
 use App\Http\Controllers\API\TransactionController;
 
@@ -18,7 +19,7 @@ use App\Http\Controllers\API\TransactionController;
 */
 
 // bisa diakses jika sudah login dengan memberikan token
-Route::middleware('auth:sanctum')->group(function() {
+Route::middleware('auth:sanctum')->group(function () {
     Route::get('user', [UserController::class, 'fetch']);
     Route::post('user', [UserController::class, 'updateProfile']);
     Route::post('user/photo', [UserController::class, 'updatePhoto']);
@@ -35,3 +36,5 @@ Route::post('login', [UserController::class, 'login']);
 Route::post('register', [UserController::class, 'register']);
 
 Route::get('food', [FoodController::class, 'all']);
+
+Route::post('midtrans/callback', [MidtransController::class, 'callback']);
