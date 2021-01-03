07d9de6c329d28b9788cf19490c889165303fd1f
Nailul Firdaus <nail.nafir@gmail.com>
2021-01-03 10:41:17 +0700

    membuat middleware IsAdmin

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,10 +2,10 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\IsAdmin;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
-class Kernel extends HttpKernel
-{
+class Kernel extends HttpKernel {
     /**
      * The application's global HTTP middleware stack.
      *
@@ -62,5 +62,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'admin' => IsAdmin::class
     ];
 }
